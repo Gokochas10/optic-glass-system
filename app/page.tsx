bdd804862cbfb5a7829ebaf1d4753dc7c781dd15
Gokochas10 <sebas.herrera3152@gmail.com>
2025-08-30 20:51:14 -0500

    Adding screen blue

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main
       className={cn(
-        "flex flex-col items-center justify-center bg-gradient"
+        "flex flex-col items-center justify-center bg-gradient h-screen"
       )}
     >
       <div className=" space-y-6 text-center">
